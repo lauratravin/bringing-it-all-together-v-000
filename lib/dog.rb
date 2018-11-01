a553f57d79738fb4fb3ff99f97852dc91d8c6de0
Laura Messa Travin <laura.travin@gmail.com>
2018-11-01 23:09:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,10 +40,10 @@ class Dog
     dog.save
     dog
   end
-  
+
   def self.find_by_id(id,db)
     arr = DB[:conn].execute("SELECT * FROM dogs WHERE id = ?",id).flatten
-    Dog.new(arr[0],arr[1],arr[2])
-  end  
+    Dog.new(id: arr[0],name: arr[1], breed: arr[2])
+  end
 
 end
