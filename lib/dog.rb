93efd6324a6918a3da768232a9ced6a9860a7f26
Laura Messa Travin <laura.travin@gmail.com>
2018-11-01 22:37:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,5 +6,15 @@ class Dog
    @name = name
    @breed = breed
  end
+ def self.create_table
+   sql= <<-SQL
+   CREATE TABLE IF NOT EXTIS dogs (
+     id INTEGER PRIMARY KEY,
+     name TEXT,
+     breed TEXT
+     )
+    SQL
+    DB[:conn].execute(sql) 
+ end   
 
 end
