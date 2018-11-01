81bb25822aaf45bf6d0c4c6fa392fd9031012103
Laura Messa Travin <laura.travin@gmail.com>
2018-11-01 22:45:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,4 +21,16 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def save
+    if self.id
+      self.update
+    else
+      sql= <<-SQL
+      INSERT INTO dogs(name,breed)
+      VALUES(?,?)
+      SQL
+      DB[:conn].execute(sql,self.name,self.breed)
+    end
+  end
+
 end
