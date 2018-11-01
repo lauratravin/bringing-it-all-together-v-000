4dfe9b052bd7f2b5b0cb02e945cdfdeb319c6661
Laura Messa Travin <laura.travin@gmail.com>
2018-11-01 22:47:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,7 +30,7 @@ class Dog
       VALUES(?,?)
       SQL
       DB[:conn].execute(sql,self.name,self.breed)
-      @id=DB[:conn].execute("SELECT last_insertrowid() FROM dogs")[0][0]
+      @id=DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
   end
 
