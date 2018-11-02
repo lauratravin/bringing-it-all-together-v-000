b2433a0cbcad85b7248c814c3ba5de8f884930f5
Laura Messa Travin <laura.travin@gmail.com>
2018-11-02 00:35:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,7 +60,7 @@ class Dog
   end
 
   def self.new_from_db(row)
-     new_dog = Dog.new(id: row[0], name: row[1], breed: [2])
+     new_dog = Dog.new(id: row[0], name: row[1], breed: row[2])
      new_dog  
   end
 
